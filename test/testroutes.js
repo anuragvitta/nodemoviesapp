4e6d421281005b587d4e9bd176e56f29db023db1
anuragvitta <33395979+anuragvitta@users.noreply.github.com>
2018-02-15 16:19:31 +0530

    Update testroutes.js

--- test/testroutes.js
+++ test/testroutes.js
@@ -33,3 +33,31 @@ it('should add movie', function(done) {
     });
 });
 });
+describe('add movie', () => {
+  const agent = request.agent(server);
+it('should add movie', function(done) {
+  chai.request(server)
+    .put('/movies/updatemovie')
+    .send({name:'henrique',id:15,language:'english',release_date:'20-07-2017'})
+    .end(function(err, res){
+      res.should.have.status(200);
+      res.should.be.json;
+      res.body.should.be.a('array');
+      done();
+    });
+});
+});
+describe('add movie', () => {
+  const agent = request.agent(server);
+it('should add movie', function(done) {
+  chai.request(server)
+    .post('/movies/getmovie')
+    .send({name:'henrique',id:15,language:'english',release_date:'20-07-2017'})
+    .end(function(err, res){
+      res.should.have.status(200);
+      res.should.be.json;
+      res.body.should.be.a('array');
+      done();
+    });
+});
+});
